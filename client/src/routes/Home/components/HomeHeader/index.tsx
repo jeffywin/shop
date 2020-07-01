a0d23ccd6bfc4090faa02b4a42f762a41517a473
jeffywin <jeffywin@126.com>
2020-07-02 06:38:41 +0800

    header

--- client/src/routes/Home/components/HomeHeader/index.tsx
+++ client/src/routes/Home/components/HomeHeader/index.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./index.less";
 interface Props {}
 function HomeHeader(props: Props) {
-  return <div>HomeHeader</div>;
+  return (
+    <header className="home-header">
+      <div className="logo-header">
+        <img src="//mcdn.pinduoduo.com/home/static/images/logo.png" alt=""/>
+      </div>
+    </header>
+  );
 }
 export default HomeHeader;
