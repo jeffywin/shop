cacf8172dadfd6093adc4f00dc29c205cb269a53
jeffywin <jeffywin@126.com>
2020-07-01 08:16:12 +0800

    home index

--- client/src/routes/Home/index.tsx
+++ client/src/routes/Home/index.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {PropsWithChildren} from "react";
 import "./index.less";
+import { RouteComponentProps } from 'react-router-dom';
 import { CombinedState } from "@/types/state";
 import { connect } from "react-redux";
 import { HomeState } from "../../types/state";
 import mapDispatchToProps from "@/store/actions/home";
 import HomeHeader from "./components/HomeHeader";
-interface Props {}
+type Props = PropsWithChildren<RouteComponentProps & ReturnType<typeof mapStateToProps> & mapDispatchToProps>
+// Props有三个属性组成，RouteComponentProps 路由属性，ReturnType<typeof mapStateToProps> 相当于 HomeState
 
 function Home(props: Props) {
   return (
@@ -15,5 +17,6 @@ function Home(props: Props) {
   );
 }
 
+// 传入老状态  state: CombinedState  =》 返回新状态 state.home
 const mapStateToProps = (state: CombinedState): HomeState => state.home;
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
