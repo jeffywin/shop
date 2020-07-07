849be95dc639d197997254fd3a950c858eae12eb
jeffywin <jeffywin@126.com>
2020-07-07 08:26:51 +0800

    feat: server

---
+++ server/readme.md
@@ -0,0 +1,4 @@
+npm init -y
+npx tsconfig.json
+npm i express mongoose body-parser bcryptjs jsonwebtoken morgan cors validator helmet dotenv multer -S
+npm i typescript  @types/node @types/express @types/mongoose @types/bcryptjs @types/jsonwebtoken  @types/morgan @types/cors @types/validator ts-node-dev nodemon  @types/helmet @types/multer -D
---
+++ server/src/index.ts
@@ -0,0 +1,31 @@
+import express, {Express} from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import path from 'path';
+import helmet from 'helmet';
+import 'dotenv/config';
+import morgan from 'morgan';
+import { urlencoded } from 'body-parser';
+
+const app: Express = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(helmet());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(urlencoded({extended: true}));
+
+app.get('/', (_req, res, _next) => {
+    res.json({
+        success: true,
+        data: 'hello jeffywin'
+    })
+});
+
+(async function(){
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => {
+        console.log('running in http:// localhost: 8001')
+    })
+})()
